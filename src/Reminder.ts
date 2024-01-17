5641f799b07d1559b0f65409644fae828bd7af6a
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-01-16 19:13:17 -0800

    start a new project

---
+++ src/Reminder.ts
@@ -0,0 +1,72 @@
+/**
+ * @class Reminder
+ * @description Represents a reminder for a given task
+ */
+export default class Reminder {
+  private _description!: string;
+  private _tag!: string;
+  private _isCompleted: boolean = false;
+
+  /*
+    Minimum Number of Properties you will need:
+    
+    _description: string
+    _tag: string
+    _isCompleted: boolean (default value should be false)
+
+    */
+
+  /**
+   * Creates a new Reminder instance.
+   * @param description - The full description of reminder
+   * @param tag - The keyword used to help categorize reminder
+   */
+  constructor(description: string, tag: string) {
+    this._description = description;
+    this._tag = tag
+  }
+
+  /**
+   * Returns the description of this reminder.
+   */
+  public get description(): string {
+    return this._description;
+    }
+
+  /**
+   * Sets the description of this reminder.
+   * @param description - The full description of reminder.
+   */
+  public set description(description: string) {
+    this._description = description;
+  }
+
+  /**
+   * Returns the tag associated with this reminder.
+   */
+  public get tag(): string {
+    return this._tag;
+  }
+
+  /**
+   * Sets the tag associated with this reminder.
+   * @param tag - The keyword used to help categorize this reminder
+   */
+  public set tag(tag: string) {
+    this._tag = tag;
+  }
+
+  /**
+   * Returns true if reminder is completed, false otherwise.
+   */
+  public get isCompleted(): boolean {
+    return this._isCompleted = false;
+  }
+
+  /**
+   * Toggles reminder completion status true ⬅ ➡ false.
+   */
+  public toggleCompletion(): void {
+    throw new Error('Not yet implemented');
+  }
+}
---
+++ src/RemindersHandler.ts
@@ -0,0 +1,163 @@
+import Reminder from './Reminder';
+
+/**
+ * A grouping of reminders based on tag (case-insensitive)
+ */
+export interface RemindersGroupingByTag {
+    [tag: string]: Reminder[];
+}
+
+/**
+ * @class RemindersHandler
+ * @description Represents a handler that manages a list of reminders
+ */
+export default class RemindersHandler {
+    private _reminders: Reminder[];
+
+    /**
+     * Creates a new RemindersHandler instance with no reminders.
+     */
+    constructor() {
+        this._reminders = [];
+    }
+
+    /**
+     * Returns the list of reminders added so far.
+     */
+    public getReminders(): Reminder[] {
+        return this._reminders;
+    }
+
+    /**
+     * Creates a new reminder and adds it to list of reminders.
+     * @param description - The full description of reminder
+     * @param tag - The keyword used to help categorize reminder
+     */
+    public addReminder(description: string, tag: string): void {
+        const reminder = new Reminder (
+            description, 
+            tag
+        )
+        this._reminders.push(reminder);
+    }
+
+    /**
+     * Returns the reminder at specified index.
+     * @throws ReminderError if specified index is not valid
+     * @param index - The index of the reminder
+     */
+    public getReminder(index: number): Reminder {
+        throw new Error("Not yet implemented");
+    }
+
+    /**
+     * Returns true if specified index is valid, false otherwise.
+     * @param index - The position of the reminder in list of reminders
+     */
+        public isIndexValid(index: number): boolean {
+        if (this.size() === 0) return false;
+        if (index < 0 || index + 1 > this.size()) return false;
+        return true;
+    }
+
+    /**
+     * Returns the number of reminders added so far.
+     */
+    public size(): number {
+        return this._reminders.length;
+    }
+
+    /**
+     * Modifies the description of the reminder at a specified index.
+     * Silently ignores call if index is not valid.
+     * @param index - The index of the reminder
+     * @param description - The full description of reminder
+     * @param tag - The keyword used to help categorize reminder
+     */
+    public modifyReminder(index: number, description: string): void {
+        this._reminders[index - 1 ].description = description;
+    }
+
+    /**
+     * Toggle the completion status of the reminder at specified index.
+     * Silently ignores call if index is not valid.
+     * @param index - The index of the reminder
+     */
+    public toggleCompletion(index: number): void {
+        this._reminders[index - 1].toggleCompletion();
+    }
+    /**
+     * Returns a list of reminders that match the keyword
+     * All reminders with tags that match the search keyword exactly will be returned first.
+     * If none exist, then all reminders with descriptions that match the search keyword (even partially)
+     * are returned.
+     * @param keyword - Text to search for in description and tag
+     */
+    public search(keyword: string): Reminder[] {
+        const list = this.searchTags(keyword);
+        if(list.length > 0) {
+            return list;
+        } 
+        return this.searchDescriptions(keyword)
+    }
+
+    /**
+     * Returns a grouping of the reminders based on tag (case-insensitive).
+     */
+    public groupByTag(): RemindersGroupingByTag {
+        const groupings: RemindersGroupingByTag = {};
+        this._reminders.forEach(reminder => {
+            if(groupings[reminder.tag] === undefined) {
+                groupings[reminder.tag] = [reminder];
+            } else {
+                groupings[reminder.tag].push(reminder);
+            }
+        })
+
+        /* Pseudocode:
+        You must group the reminders by tags. 
+        So you need to take this._reminders which is currently like so:
+
+        [{"tag": grocery, }, {reminder2}, {reminder3}, {reminder4}]
+        
+        And transform it into a groupings object
+        like this:
+
+        {
+            "coding": [{ reminder1 }, { reminder2 }],
+            "grocery": [{ reminder3 }, { reminder4 }],
+        }
+
+        */
+       
+        return groupings;
+    }
+
+    /**
+     * Returns a list of reminders with tags that match the keyword exactly.
+     * @param keyword - Text to search for in description and tag
+     */
+    private searchTags(keyword: string): Reminder[] {
+        let checkTag = [];
+            for (const reminder of this._reminders) {
+                if(reminder.tag === keyword) {
+                    checkTag.push(reminder);
+                }
+            }
+            return checkTag;
+    }
+
+    /**
+     * Returns a list of reminders with descriptions that match the keyword.
+     * @param keyword - Text to search for in description and tag
+     */
+    private searchDescriptions(keyword: string): Reminder[] {
+        let checkDescription = [];
+            for (const reminder of this._reminders) {
+                if(reminder.description.includes(keyword)) {
+                    checkDescription.push(reminder);
+                }
+            }
+            return checkDescription;
+    }
+}
